000acb243e24e71371784a6caaa3bac08e56e08b
Mehdi <labiad.elmehdi@gmail.com>
2023-04-12 09:47:15 +0200

    starting about page

--- kasa-react/src/pages/About.jsx
+++ kasa-react/src/pages/About.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+//import Banner from "../components/Banner"
+import AboutBanner from "../images/landscape_one.png"
+import "../styles/About.css"
+
+/*Impossible d'utiliser le composant banner, la taille en hauteur native pour mobile
+et différente sur la page Home (je continue d'essayer de trouver une solution
+pour pouvoir avoir le même composant (Banner) mais un CSS différent pour
+la page About"*/
 
 const About = () => {
   return (
-    <div>About</div>
+    <div className='about'>
+      <div className='about-banner'>
+        <img src={AboutBanner} alt="bannière"/>
+      </div>
+    </div>
   )
 }
 
